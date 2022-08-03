8d5da70c52f450e05f050a6e32e96fb14321ab99
luccyis <mungjee08@naver.com>
2022-08-03 17:25:08 +0900

    operator

--- src/com/mycompany/study/ch03/ArithmethicOperatorExample.java
+++ src/com/mycompany/study/ch03/ArithmethicOperatorExample.java
@@ -38,12 +38,39 @@ public class ArithmethicOperatorExample {
 		System.out.println("result7:" + result7);
 		
 		
+		double a = ((1 + 2/3) - (5/8)) * (9 + 4/5);
+		System.out.println(a);
+		
+		double b = 5 - (3.25 - (1+5/6))*(12/17);
+		System.out.println(b);
 		
 		
+		sum(8,7,5);
+		sum(1,9,5);
+		sum(5,6,6);
+		sum(4,7,1);
+		sum(7,5,5);
 		
-		
+		subAvg("홍길동", 60,70,85,90,45);
+		subAvg("박말순", 50,75,70,60,70);
+		subAvg("고장난", 55,60,64,58,90);
+		subAvg("도롱뇽", 80,70,63,88,78);
 		
 		
 	}
+	
+	//추가되는 함수는 메인함수 밖, 클래스 안 에
+	//자바는 변수명 풀네임으로
+	
+	public static void sum(int a, int b, int c) {
+		System.out.println(a + "+" + b + "+" + c + "=" + (a+b+c));
+	}
+	
+	public static void subAvg(String name, int english, int math, int physics, int music, int athletic) {
+		System.out.println(name + " 평균: " + (double)(english + math + physics + music + athletic)/5);
+	}
+		
+	
+	
 
 }
---
+++ src/com/mycompany/study/ch03/StringConcatExample.java
@@ -0,0 +1,15 @@
+package com.mycompany.study.ch03;
+
+public class StringConcatExample {
+
+	public static void main(String[] args) {
+
+		String stringValue = "jdk";
+		String stringValue2 = "1.1";
+		
+		System.out.println("우리가 사용하는 " + stringValue + " " + stringValue2);
+		
+		
+	}
+
+}
