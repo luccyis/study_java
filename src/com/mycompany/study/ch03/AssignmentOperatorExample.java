c0c97b67d0c5db464f7aa8dc413ab0675668a5df
luccyis <mungjee08@naver.com>
2022-08-05 13:50:38 +0900

    operator

---
+++ src/com/mycompany/study/ch03/AssignmentOperatorExample.java
@@ -0,0 +1,31 @@
+package com.mycompany.study.ch03;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+
+// 		+=
+		
+		int result = 0;
+		int result2 = 0;
+		int result3 = 0;
+		
+		result += 10;
+		//result = result+ 10;
+		System.out.println("result: " + result);
+		
+// 		-=
+		
+		result2 -= 4;
+		//result = result - 4;
+		System.out.println("result2: " + result2);
+		
+// 		*=
+		
+		result3 *= 6;
+		//result = result * 6;
+		System.out.println("result3: " + result3);
+		
+	}
+
+}
